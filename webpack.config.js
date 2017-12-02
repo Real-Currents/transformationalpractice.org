65ce5cf8868f78d572e028b34d9bd537423e272c
John Hall <john@real-currents.com>
2017-12-02 12:23:17 -0500

    migrated webpack and sass loader

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,39 @@
-var webpack = require('webpack');
-var path = require('path');
+const webpack = require('webpack');
+const path = require('path');
+const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+const pkg = require(__dirname + '/package.json');
+const sassLoaders = [
+  'file-loader?name=../css/[name].css',
+  'postcss-loader',
+  'sass-loader?sourceMap'
+];
+const scriptsPath = path.resolve(__dirname, 'build', 'scripts');
+const sitejs = ['./scripts/site.js'];
+const sourceScripts = sitejs;
+const IS_PRODUCTION = true; // = process.env.NODE_ENV === 'production';
 
-var scriptsPath = path.resolve(__dirname, 'build', 'scripts');
-
-var sitejs = ['./scripts/site.js'];
-
-var sourceScripts = sitejs;
-
-var config = {
-  entry: sourceScripts,
+const config = {
+  devtool: IS_PRODUCTION ? false : 'source-map',
+  entry: {
+    'scripts/site.bundle': sourceScripts,
+    'styles/custom': [ './sass/custom.scss' ]
+  },
   output: {
     path: scriptsPath,
-    filename: 'site-bundle.js'
+    filename: '[name].js'
   },
   module: {
-    loaders: [
-      {
+    rules: [
+      { // CSS
+        test: /\.css$/,
+        loader: ExtractTextPlugin.extract(['css-loader?importLoaders=1'])
+      },
+      { // Sass
+        test: /\.(sass|scss)$/,
+        loader: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
+      },
+      { // JavaScript
         test: /\.js$/,
         exclude: /node_modules/,
         loader: 'babel-loader'
@@ -23,10 +41,35 @@ var config = {
     ]
   },
   plugins: [
-    new webpack.optimize.UglifyJsPlugin({
-      output: {
-        comments: false
-      }
+        // new webpack.optimize.UglifyJsPlugin({
+        //     output: {
+        //         comments: false
+        //     }
+        // }),
+        //
+        // new webpack.optimize.UglifyJsPlugin({
+        //     beautify: !IS_PRODUCTION,
+        //     compress: IS_PRODUCTION ? {
+        //         drop_console: true, // eslint-disable-line camelcase
+        //         warnings: false
+        //     } : false,
+        //     mangle: IS_PRODUCTION ? {
+        //         except: ['_'] // don't mangle lodash
+        //     } : false
+        // }),
+
+    new BrowserSyncPlugin({
+            // browse to http://localhost:3000/ during development,
+            // ./public directory is being served
+      host: 'localhost',
+      port: 3000,
+      proxy: 'localhost:9000',
+      serveStatic: ['./styles']
+    }),
+
+    new ExtractTextPlugin({ // define where to save the file
+      filename: '[name].css',
+      allChunks: true
     })
   ]
 };
