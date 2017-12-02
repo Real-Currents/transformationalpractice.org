777e834ef669bdd6d08f83642a3230e919dbe2d1
John Hall <john@real-currents.com>
2017-12-02 12:49:14 -0500

    improve webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,47 @@
 const webpack = require('webpack');
+const fs = require('fs');
 const path = require('path');
 const BrowserSyncPlugin = require('browser-sync-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const pkg = require(__dirname + '/package.json');
 const sassLoaders = [
-  'file-loader?name=../css/[name].css',
-  'postcss-loader',
-  'sass-loader?sourceMap'
+    'file-loader?name=../css/[name].css',
+    'postcss-loader',
+    'sass-loader?sourceMap'
 ];
-const scriptsPath = path.resolve(__dirname, 'build', 'scripts');
+const scriptsPath = path.resolve(__dirname, 'build');
 const sitejs = ['./scripts/site.js'];
 const sourceScripts = sitejs;
 const IS_PRODUCTION = true; // = process.env.NODE_ENV === 'production';
 
 const config = {
-  devtool: IS_PRODUCTION ? false : 'source-map',
-  entry: {
-    'scripts/site.bundle': sourceScripts,
-    'styles/custom': [ './sass/custom.scss' ]
-  },
-  output: {
-    path: scriptsPath,
-    filename: '[name].js'
-  },
-  module: {
-    rules: [
-      { // CSS
-        test: /\.css$/,
-        loader: ExtractTextPlugin.extract(['css-loader?importLoaders=1'])
-      },
-      { // Sass
-        test: /\.(sass|scss)$/,
-        loader: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
-      },
-      { // JavaScript
-        test: /\.js$/,
-        exclude: /node_modules/,
-        loader: 'babel-loader'
-      }
-    ]
-  },
-  plugins: [
+    devtool: IS_PRODUCTION ? false : 'source-map',
+    entry: {
+        'scripts/site-bundle': sourceScripts,
+        'styles/custom': ['./sass/custom.scss']
+    },
+    output: {
+        path: scriptsPath,
+        filename: '[name].js'
+    },
+    module: {
+        rules: [
+            { // CSS
+                test: /\.css$/,
+                loader: ExtractTextPlugin.extract(['css-loader?importLoaders=1'])
+            },
+            { // Sass
+                test: /\.(sass|scss)$/,
+                loader: ExtractTextPlugin.extract(['css-loader', 'sass-loader'])
+            },
+            { // JavaScript
+                test: /\.js$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            }
+        ]
+    },
+    plugins: [
         // new webpack.optimize.UglifyJsPlugin({
         //     output: {
         //         comments: false
@@ -58,20 +59,23 @@ const config = {
         //     } : false
         // }),
 
-    new BrowserSyncPlugin({
+        new BrowserSyncPlugin({
             // browse to http://localhost:3000/ during development,
             // ./public directory is being served
-      host: 'localhost',
-      port: 3000,
-      proxy: 'localhost:9000',
-      serveStatic: ['./styles']
-    }),
+            host: 'localhost',
+            port: 3000,
+            proxy: 'localhost:9000',
+            serveStatic: ['build/styles'],
+            browser: (fs.existsSync('C:\\Program Files\\Firefox Developer Edition\\firefox.exe'))?
+                'C:\\Program Files\\Firefox Developer Edition\\firefox.exe' :
+                'firefox'
+        }),
 
-    new ExtractTextPlugin({ // define where to save the file
-      filename: '[name].css',
-      allChunks: true
-    })
-  ]
+        new ExtractTextPlugin({ // define where to save the file
+            filename: '[name].css',
+            allChunks: true
+        })
+    ]
 };
 
 module.exports = config;
